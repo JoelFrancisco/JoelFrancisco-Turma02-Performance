7529c8ff7f6b233bf23a76b0be82edfd9c93d411
Joel Francisco Da Silva Filho <joel.488714@alunosatc.edu.br>
2024-11-28 20:26:43 -0300

    test: testando actions

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -4,18 +4,30 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+export const getThemesDuration = new Trend('get_themes', true);
+export const successRate = new Rate('success_rate');
+
+const baseUrl = 'https://api.maribel.cloud';
+
+let params = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_failed: ['rate<0.12'],
+    get_themes: ['p(95)<5700'],
+    success_rate: ['rate>0.95'] 
   },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
+    { duration: '15s', target: 10 },
+    { duration: '15s', target: 20 },
+    { duration: '30s', target: 50 },
+    { duration: '30s', target: 100 },
+    { duration: '1m', target: 200 },
+    { duration: '2m', target: 300 }
   ]
 };
 
@@ -26,24 +38,31 @@ export function handleSummary(data) {
   };
 }
 
-export default function () {
-  const baseUrl = 'https://test.k6.io/';
+function getToken() {
+  const username = __ENV.USERNAME;
+  const password = __ENV.PASSWORD;
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  const body = JSON.stringify({ username, password });
+
+  const res = http.post(`${baseUrl}/api/auth/jwt/create/`, body, params);
+  const json = res.json();
+  return json.access;
+}
+
+export default function () {
+  const accessToken = getToken();
+
+  params.headers["Authorization"] = `Bearer ${accessToken}`;
 
   const OK = 200;
 
-  const res = http.get(`${baseUrl}`, params);
+  const res = http.get(`${baseUrl}/api/themes`, params);
 
-  getContactsDuration.add(res.timings.duration);
+  getThemesDuration.add(res.timings.duration);
 
-  RateContentOK.add(res.status === OK);
+  successRate.add(res.status === OK);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'GET Themes - Status 200': () => res.status === OK
   });
 }
